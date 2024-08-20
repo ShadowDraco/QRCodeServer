12172ca5b39bb52d2a7652510bea6728be2a44fa
Ethan Storm <github.balsamic226@passinbox.com>
2024-08-20 15:55:09 -0700

    add support page, and begin basic branding and readme

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const app = express()
+require("dotenv").config();
+const { rateLimit } = require("express-rate-limit");
+const express = require("express");
+const cors = require("cors");
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 50,
+  standardHeaders: "draft-7",
+  legacyHeaders: false,
+});
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-const PORT = process.env.PORT
+const app = express();
 
-const QRRouter = require('./routes/qr')
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(limiter);
 
-app.get('/', (req, res) => {
-  res.send('')
-})
+const PORT = process.env.PORT;
 
-app.post('/lol', (req, res) => {
-  console.log('BODY ', req.body)
-  res.json(req.body)
-})
+const QRRouter = require("./routes/qr");
 
-app.use('/qr', QRRouter)
+app.get("/", (req, res) => {
+  res.send("");
+});
+
+app.use("/qr", QRRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
