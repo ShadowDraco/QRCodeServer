16d0b2a2162dbe1c7ec27139f0b43ed7eb944689
Ethan Storm <github.balsamic226@passinbox.com>
2024-08-29 18:25:51 -0700

    add stats, and make all functions run through database

--- routes/qr.js
+++ routes/qr.js
@@ -10,38 +10,42 @@ const {
   checkForBadURLS,
   getAllQRsFromDatabase,
   getGroupFromDatabase,
+  getStatsFromDatabase,
   detectDuplicateQR,
+  adminCreateStats,
+  updateQRsInDatabase,
+  updateQRCount,
+  updateScansInDatabase,
 } = require("./lib");
-
-const allQRCodes = [];
-let allTimeQRs = 0;
-let allTimeScans = 0;
 
 //? call once to clear database
 //adminDeleteAllQRs();
-
+//? Call once to create stats in database (needs to be done if the stats are completely deleted)
+//adminCreateStats();
 router.get("/all", async (req, res) => {
-  //const returnQRs = allQRCodes.filter((qr) => !qr.protected);
   const returnQRs = await getAllQRsFromDatabase();
+  const [allTimeQRs, allTimeScans] = await getStatsFromDatabase();
   res.json({ allQRS: returnQRs, allTimeQRs, allTimeScans });
 });
 
-router.post("/code", (req, res) => {
+router.post("/code", async (req, res) => {
   const code = req.body.code;
   const returnQRs = getGroupFromDatabase(code);
-
+  const [allTimeQRs, allTimeScans] = await getStatsFromDatabase();
   res.json({ codeQRs: returnQRs, allTimeQRs, allTimeScans, erorr: "" });
 });
 
-router.get("/visit/:url", async (req, res) => {
+router.get("/visit/:url/:code", async (req, res) => {
   let visitUrl = req.params.url;
   let decodedUrl = decodeURIComponent(visitUrl);
+  let visitCode = req.params.code;
+  let decodedCode = decodeURIComponent(visitCode);
 
-  let QR = await QRModel.find((QR) => QR.url == decodedUrl);
+  let QR = await QRModel.find({ url: decodedUrl, code: decodedCode });
 
   if (QR) {
-    QR.count += 1;
-    allTimeScans += 1;
+    updateQRCount(QR);
+    updateScansInDatabase();
     res.redirect(`${QR.url}`);
   } else {
     res.json({ message: "This redirect does not exist.", error: "" });
@@ -49,9 +53,8 @@ router.get("/visit/:url", async (req, res) => {
 });
 
 router.post("/create", async (req, res) => {
-  if (!req.body) {
-    res.json({ message: "Something is wrong", error: "No request body" });
-  }
+  // if there is an error don't create qr
+  let hasError = false;
 
   let returnData = {
     message: "",
@@ -59,49 +62,55 @@ router.post("/create", async (req, res) => {
     error: "",
   };
 
+  if (!req.body) {
+    hasError = true;
+    returnData.error = "Bad Request";
+  }
+
   const createUrl = req.body.url;
   const createCode = req.body.code;
   const createProtected = req.body.protected;
 
   // if bad/nsfw url
   if (checkForBadURLS(createUrl, createCode)) {
+    hasError = true;
     returnData.error = "Do not create NSFW QRS";
-    res.json(returnData);
   }
   // if duplicate url
-  if (detectDuplicateQR(createUrl, createCode)) {
+  if (await detectDuplicateQR(createUrl, createCode)) {
+    hasError = true;
     returnData.error = "You cannot create duplicate QR's";
-    res.json(returnData);
   }
   // create qr
-  try {
-    let encodedUrl = encodeURIComponent(createUrl);
-
-    QRCode.toDataURL(
-      `${process.env.URL}/qr/visit/${encodedUrl}`,
-      function (err, url) {
-        newQR = {
-          qr: url,
-          url: createUrl,
-          count: 0,
-          code: createCode,
-          protected: createProtected,
-        };
-        allQRCodes.push(newQR);
-        saveQRToDatabase(newQR);
-        returnData.QR = newQR;
-        returnData.message = "Successful!";
-        allTimeQRs += 1;
-        returnData.allTimeScans = allTimeScans;
-        returnData.allTimeQRs = allTimeQRs;
-        res.json(returnData);
-      }
-    );
-  } catch (err) {
-    console.error("Error creating QR Code", err);
-    returnData.error = err;
-    res.json(returnData);
+  if (!hasError) {
+    try {
+      let encodedUrl = encodeURIComponent(createUrl);
+      let encodedCode = encodeURIComponent(createCode);
+      QRCode.toDataURL(
+        `${process.env.URL}/qr/visit/${encodedUrl}/${encodedCode}`,
+        function (err, url) {
+          newQR = {
+            qr: url,
+            url: createUrl,
+            count: 0,
+            code: createCode,
+            protected: createProtected,
+          };
+          saveQRToDatabase(newQR);
+          updateQRsInDatabase();
+          returnData.QR = newQR;
+          returnData.message = "Successful!";
+        }
+      );
+    } catch (err) {
+      console.error("Error creating QR Code", err);
+      returnData.error = err;
+    }
   }
+  const [allTimeQRs, allTimeScans] = await getStatsFromDatabase();
+  returnData.allTimeQRs = allTimeQRs;
+  returnData.allTimeScans = allTimeScans;
+  res.json(returnData);
 });
 
 module.exports = router;
