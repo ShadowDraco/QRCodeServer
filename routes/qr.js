55c4faeb56900a77cfd6a3111ef824714c4d6b84
Ethan Storm <github.balsamic226@passinbox.com>
2024-09-15 14:25:08 -0700

    update duplicate logic, fix crash on duplicate
    Sending a duplicate QR would cause the server to crash due to double response to the client. Add an if to the duplicate response cured that

--- routes/qr.js
+++ routes/qr.js
@@ -43,39 +43,39 @@ router.post("/create", async (req, res) => {
   const createProtected = req.body.protected;
 
   // if duplicate url
-  if (allQRCodes.find((QR) => QR.url == createUrl)) {
+  if (allQRCodes.find((QR) => QR.url == createUrl && QR.code == createCode)) {
     console.error("Duplicate QR");
-    returnData.message = "You cannot create duplicate QR's";
-    res.json();
-  }
-
-  // create qr
-  try {
-    let encodedUrl = encodeURIComponent(createUrl);
-
-    QRCode.toDataURL(
-      `${process.env.URL}/qr/visit/${encodedUrl}`,
-      function (err, url) {
-        newQR = {
-          qr: url,
-          url: createUrl,
-          count: 0,
-          code: createCode,
-          protected: createProtected,
-        };
-        allQRCodes.push(newQR);
-        returnData.QR = newQR;
-        returnData.message = "Successful!";
-        allTimeQRs += 1;
-        returnData.allTimeScans = allTimeScans;
-        returnData.allTimeQRs = allTimeQRs;
-        res.json(returnData);
-      }
-    );
-  } catch (err) {
-    console.error("Error creating QR Code", err);
-    returnData.error = err;
+    returnData.error = "You cannot create duplicate QR's";
     res.json(returnData);
+  } else {
+    // create qr
+    try {
+      let encodedUrl = encodeURIComponent(createUrl);
+
+      QRCode.toDataURL(
+        `${process.env.URL}/qr/visit/${encodedUrl}`,
+        function (err, url) {
+          newQR = {
+            qr: url,
+            url: createUrl,
+            count: 0,
+            code: createCode,
+            protected: createProtected,
+          };
+          allQRCodes.push(newQR);
+          returnData.QR = newQR;
+          returnData.message = "Successful!";
+          allTimeQRs += 1;
+          returnData.allTimeScans = allTimeScans;
+          returnData.allTimeQRs = allTimeQRs;
+          res.json(returnData);
+        }
+      );
+    } catch (err) {
+      console.error("Error creating QR Code", err);
+      returnData.error = err;
+      res.json(returnData);
+    }
   }
 });
 
