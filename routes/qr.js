aa21d092ccb8471f27fe2785898731a82a1a1f98
Ethan Storm <github.balsamic226@passinbox.com>
2024-09-15 14:25:08 -0700

    add url decoding, and require validated url

--- routes/qr.js
+++ routes/qr.js
@@ -10,17 +10,20 @@ router.get('/all', (req, res) => {
 
 router.get('/visit/:url', (req, res) => {
   let visitUrl = req.params.url
-  let QR = AllQRCodes.find(QR => QR.url == visitUrl)
-  console.log(visitUrl, QR, AllQRCodes)
+  let decodedUrl = decodeURIComponent(visitUrl)
+  console.log(visitUrl, decodedUrl)
+  let QR = AllQRCodes.find(QR => QR.url == decodedUrl)
+
   if (QR) {
     QR.count += 1
-    res.redirect(`https://${QR.url}`)
+    res.redirect(`${QR.url}`)
   } else {
     res.json({ message: 'This redirect does not exist.', error: '' })
   }
 })
 
 router.post('/create', async (req, res) => {
+
   const createUrl = req.body.url
 
   if (!req.body) {
@@ -35,13 +38,16 @@ router.post('/create', async (req, res) => {
   }
   // create qr
   try {
+    let encodedUrl = encodeURIComponent(createUrl)
+    console.log(encodedUrl)
     QRCode.toDataURL(
-      `${process.env.URL}/qr/visit/${createUrl}`,
+      `${process.env.URL}/qr/visit/${encodedUrl}`,
       function (err, url) {
         newQR = { qr: url, url: createUrl, count: 0 }
         AllQRCodes.push(newQR)
         returnData.QR = newQR
         returnData.message = 'Successful!'
+        console.log(returnData)
         res.json(returnData)
       }
     )
